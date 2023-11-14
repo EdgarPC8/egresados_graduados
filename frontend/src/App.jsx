e2739f1af823457b7f67099eb7f9e8d898a211ff
Edgar Torres <edgartorrespc8@gmail.com>
2023-11-14 10:52:12 -0500

    actualizacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import customTheme from "./theme/theme.jsx";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
+import CV from "./pages/CV.jsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/curriculums" element="Lista Docentes" />
           <Route path="/inicio" element={<HomePage/>} />
+          <Route path="/cv" element={<CV/>} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
