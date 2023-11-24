4394d770a737875338dcf662f424042659451df2
Edgar Torres <edgartorrespc8@gmail.com>
2023-11-23 20:49:31 -0500

    nuevos cambios

--- frontend/src/api/dataRequest.js
+++ frontend/src/api/dataRequest.js
@@ -3,4 +3,6 @@ import axios from "./axios.js";
 
 const getData = async (data) => await axios.post("/data/select", data);
 const setData = async (data) => await axios.post("/data/insert", data);
-export { getData,setData };
+const insertStudent = async (data) => await axios.post("/students/insertStudent", data);
+
+export { getData,setData,insertStudent };
