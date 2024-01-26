8a8ba7fc42e3d26e93be0b37c14171865c1a9e88
Edgar Torres <edgartorrespc8@gmail.com>
2024-01-25 21:16:15 -0500

    alumni 1.0.6

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://192.168.137.250:3000/api",
-  // baseURL: "http://aplicaciones.marianosamaniego.edu.ec:3000/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://aplicaciones.marianosamaniego.edu.ec:3000/api",
+  // baseURL: "http://localhost:3000/api",
   // baseURL: "http://192.168.137.250:3000/api",
   withCredentials: true,
 });
@@ -12,8 +12,8 @@ export const jwt = () => {
   return `Bearer ${window.localStorage.getItem("token")}`;
 };
 
-// export const urlPhotos = "http://aplicaciones.marianosamaniego.edu.ec:3000/photos";
-export const urlPhotos = "http://localhost:3000/photos";
+export const urlPhotos = "http://aplicaciones.marianosamaniego.edu.ec:3000/photos";
+// export const urlPhotos = "http://localhost:3000/photos";
 // export const urlPhotos = "http://192.168.137.250:3000/photos";
 
 
