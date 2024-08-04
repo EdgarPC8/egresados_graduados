bc5d92b8bcb3e4b0f31d2de8ad8528b7904ed5c3
Edgar Torres <edgartorrespc8@gmail.com>
2024-08-04 14:38:24 -0500

    cambios en sidebar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   Box,
   Button,
@@ -18,189 +18,132 @@ import {
   DrawerContent,
   DrawerCloseButton,
   Badge,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
 } from "@chakra-ui/react";
-import { useRef } from "react";
-
 import { Link } from "react-router-dom";
-
-import NavLink from "./NavLink";
-
-import {
-  FiHome,
-  FiFile,
-  FiAward,
-  FiFileText,
-  FiUser,
-  FiLogOut,
-  FiTarget,
-} from "react-icons/fi";
-import { FaYoutube } from "react-icons/fa";
-import { PiFiles } from "react-icons/pi";
-
+import { FiHome, FiFile, FiAward, FiUser, FiLogOut, FiTarget } from "react-icons/fi";
+import { FaChartPie } from "react-icons/fa";
+import { TbBrandMatrix } from "react-icons/tb";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import { useAuth } from "../context/AuthContext";
 import { urlPhotos } from "../api/axios";
-import { FaChartPie } from "react-icons/fa";
-import { TbBrandMatrix } from "react-icons/tb";
+import { FaYoutube } from "react-icons/fa";
+import { PiFiles } from "react-icons/pi";
 import { CgCalendarDates } from "react-icons/cg";
 import { GiBookPile } from "react-icons/gi";
+import NavLink from "./NavLink";
+// Constantes para las vistas sin autenticación
+const LinksToNoAuth = [
+  {
+    name: "Inicio",
+    path: "/",
+    icon: <FiHome />,
+  },
+];
+
+// Constantes para las vistas con autenticación y permisos
+const permisos = {
+  Programador: [
+    {name: "Inicio",link: "/",icon: <FiHome />},
+    { name: "Hoja de Vida", link: "/cv", icon: <FiAward /> },
+    {
+      name: "Panel de Control",
+      icon: <FiTarget />,
+      menu: {
+        items: [
+          { name: "Panel", link: "/panel", icon: <FiTarget /> },
+          { name: "Actividad", link: "/actividad", icon: <FiTarget /> },
+        ],
+      },
+    },
+    {
+      name: "Entidades",
+      icon: <FiUser />,
+      menu: {
+        items: [
+          { name: "Usuarios", link: "/usuarios", icon: <FiUser /> },
+          { name: "Carreras", link: "/carreras", icon: <GiBookPile /> },
+          { name: "Periodos", link: "/periodos", icon: <CgCalendarDates /> },
+          { name: "Curriculos", link: "/curriculos", icon: <FiFile /> },
+          { name: "Graficos", link: "/charts", icon: <FaChartPie /> },
+        ],
+      },
+    },
+    {
+      name: "Encuestas",
+      icon: <PiFiles />,
+      menu: {
+        items: [
+          { name: "Todas", link: "/quizzes", icon: <PiFiles /> },
+          { name: "Encuesta", link: "/quiz", icon: <PiFiles /> },
+        ],
+      },
+    },
+    { name: "Tutoriales", link: "/tutoriales", icon: <FaYoutube  /> },
+    { name: "Matrices", link: "/matriz", icon: <TbBrandMatrix /> },
+  ],
+  Administrador: [
+    {
+      name: "Inicio",
+      link: "/",
+      icon: <FiHome />,
+    },
+    { name: "Hoja de Vida", link: "/cv", icon: <FiAward /> },
+    {
+      name: "Entidades",
+      icon: <FiHome />,
+      menu: {
+        items: [
+          // { name: "Usuarios", link: "/usuarios", icon: <FiUser /> },
+          { name: "Carreras", link: "/carreras", icon: <GiBookPile /> },
+          { name: "Periodos", link: "/periodos", icon: <CgCalendarDates /> },
+          { name: "Curriculos", link: "/curriculos", icon: <FiFile /> },
+          { name: "Graficos", link: "/charts", icon: <FaChartPie /> },
+        ],
+      },
+    },
+    {
+      name: "Encuestas",
+      icon: <PiFiles />,
+      menu: {
+        items: [
+          { name: "Todas", link: "/quizzes", icon: <PiFiles /> },
+          { name: "Encuesta", link: "/quiz", icon: <PiFiles /> },
+        ],
+      },
+    },
+    { name: "Tutoriales", link: "/tutoriales", icon: <FaYoutube  /> },
+    { name: "Matrices", link: "/matriz", icon: <TbBrandMatrix /> },
+  ],
+  Profesional: [
+    {
+      name: "Inicio",
+      link: "/",
+      icon: <FiHome />,
+    },
+    { name: "Hoja de Vida", link: "/cv", icon: <FiAward /> },
+    { name: "Encuesta", link: "/quiz", icon: <PiFiles /> },
+  ],
+  Estudiante: [
+    {
+      name: "Inicio",
+      link: "/",
+      icon: <FiHome />,
+    },
+  ],
+};
 
 const Navbar = () => {
   const btnRef = useRef();
   const { isAuthenticated, logout, user, isLoading } = useAuth();
-
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const LinksToNoAuth = [
-    {
-      name: "Inicio",
-      path: "/",
-      icon: <FiHome />,
-    },
-    // {
-    //   name: "Curriculos",
-    //   path: "/curriculos",
-    //   icon: <FiHome />,
-    // },
-  ];
-
-  const Links = {
-    Programador: [
-      {
-        name: "Panel de Control",
-        path: "/panel",
-        icon: <FiTarget />,
-      },
-      {
-        name: "Actividad",
-        path: "/actividad",
-        icon: <FiTarget />,
-      },
-      {
-        name: "Inicio",
-        path: "/",
-        icon: <FiHome />,
-      },
-      {
-        name: "Usuarios",
-        path: "/usuarios",
-        icon: <FiUser />,
-      },
-      {
-        name: "Graficos",
-        path: "/charts",
-        icon: <FaChartPie />,
-      },
-      {
-        name: "Curriculos",
-        path: "/curriculos",
-        icon: <FiFile />,
-      },
-      {
-        name: "Hoja de Vida",
-        path: "/cv",
-        icon: <FiAward />,
-      },
-      {
-        name: "Encuesta",
-        path: "/quiz",
-        icon: <FiFileText />,
-      },
-      {
-        name: "Encuestas",
-        path: "/quizzes",
-        icon: <PiFiles />,
-      },
-      {
-        name: "Matrices",
-        path: "/matriz",
-        icon: <TbBrandMatrix />,
-      },
-      {
-        name: "Carreras",
-        path: "/carreras",
-        icon: <GiBookPile />,
-      },
-      {
-        name: "Periodos",
-        path: "/periodos",
-        icon: <CgCalendarDates />,
-      },
-      {
-        name: "Tutoriales",
-        path: "/tutoriales",
-        icon: <FaYoutube />,
-      },
-    ],
-
-    Administrador: [
-      {
-        name: "Inicio",
-        path: "/",
-        icon: <FiHome />,
-      },
-      {
-        name: "Graficos",
-        path: "/charts",
-        icon: <FaChartPie />,
-      },
-      {
-        name: "Curriculos",
-        path: "/curriculos",
-        icon: <FiFile />,
-      },
-      {
-        name: "Hoja de Vida",
-        path: "/cv",
-        icon: <FiAward />,
-      },
-      {
-        name: "Encuesta",
-        path: "/quiz",
-        icon: <FiFileText />,
-      },
-      {
-        name: "Encuestas",
-        path: "/quizzes",
-        icon: <PiFiles />,
-      },
-      {
-        name: "Matrices",
-        path: "/matriz",
-        icon: <TbBrandMatrix />,
-      },
-      {
-        name: "Carreras",
-        path: "/carreras",
-        icon: <GiBookPile />,
-      },
-      {
-        name: "Periodos",
-        path: "/periodos",
-        icon: <CgCalendarDates />,
-      },
-      {
-        name: "Tutoriales",
-        path: "/tutoriales",
-        icon: <FaYoutube />,
-      },
-    ],
-    Profesional: [
-      {
-        name: "Hoja de Vida",
-        path: "/cv",
-        icon: <FiAward />,
-      },
-      {
-        name: "Encuesta",
-        path: "/quiz",
-        icon: <FiFileText />,
-      },
-    ],
-    Estudiante: [
- 
-    ],
-  };
+  // Páginas que se mostrarán según el rol del usuario
+  const pagesToShow = permisos[user?.loginRol] || [];
 
   return (
     <>
@@ -214,18 +157,12 @@ const Navbar = () => {
               ref={btnRef}
               onClick={onOpen}
             />
-
-            <HStack
-              as={"nav"}
-              spacing={4}
-              display={{ base: "none", md: "flex" }}
-            >
+            <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
               <Image
                 borderRadius="full"
                 boxSize="50px"
                 src="http://www.marianosamaniego.edu.ec/eva/pluginfile.php/1/core_admin/logo/0x200/1679244973/logoistms.jpeg"
-                // src="../public/logoistms.jpeg"
-                alt="Dan Abramov"
+                alt="Logo ISTMS"
               />
               {!isAuthenticated &&
                 LinksToNoAuth.map((link) => (
@@ -243,53 +180,65 @@ const Navbar = () => {
                   {user.loginRol}
                 </Badge>
                 <Link to="/perfil">
-                  <Avatar
-                    size={"sm"}
-                    mr="10px"
-                    src={`${urlPhotos}/${user.photo}`}
-                  >
+                  <Avatar size={"sm"} mr="10px" src={`${urlPhotos}/${user.photo}`}>
                     <AvatarBadge boxSize="1.25em" bg="green.500" />
                   </Avatar>
                 </Link>
               </Center>
             </Flex>
-          ) : isOpen ? (
-            ""
           ) : (
-            <Flex alignItems="center">
-              <HStack spacing={4}>
-                <Link to="/register">
-                  <Button>Registrarse</Button>
-                </Link>
-                <Link to="/login">
-                  <Button>Iniciar Sesión</Button>
-                </Link>
-              </HStack>
-            </Flex>
+            !isOpen && (
+              <Flex alignItems="center">
+                <HStack spacing={4}>
+                  <Link to="/register">
+                    <Button>Registrarse</Button>
+                  </Link>
+                  <Link to="/login">
+                    <Button>Iniciar Sesión</Button>
+                  </Link>
+                </HStack>
+              </Flex>
+            )
           )}
         </Flex>
       </Box>
 
-      <Drawer
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose} finalFocusRef={btnRef}>
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
-
           <DrawerBody>
             {!isLoading && isAuthenticated ? (
-              <Stack align="start" spacing={4} flex="1" mr={4}>
-                {user.loginRol &&
-                  Links[user.loginRol].map((link) => (
-                    <Link to={link.path} key={link.name} onClick={onClose}>
-                      <NavLink icon={link.icon}>{link.name}</NavLink>
-                    </Link>
-                  ))}
-              </Stack>
+              <Accordion allowToggle mt={10}>
+                {pagesToShow.map((page) =>
+                  page.menu ? (
+                    <AccordionItem key={page.name}>
+                      <h2>
+                        <AccordionButton>
+                          <Box as="span" flex="1" textAlign="left" >
+                            <NavLink type='text' icon={page.icon}>{page.name}</NavLink>
+                          </Box>
+                          <AccordionIcon />
+                        </AccordionButton>
+                      </h2>
+                      <AccordionPanel pb={4}>
+                          {page.menu.items.map((item) => (
+                            <Link to={item.link} key={item.name} onClick={onClose}>
+                            <NavLink icon={item.icon}>{item.name}</NavLink>
+                            </Link>
+                          ))}
+                      </AccordionPanel>
+                    </AccordionItem>
+                  ) : (
+                    <AccordionItem key={page.name}>
+                  
+                      <Link to={page.link} key={page.name} onClick={onClose}>
+                        <NavLink icon={page.icon}>{page.name}</NavLink>
+                      </Link>
+                    </AccordionItem>
+                  )
+                )}
+              </Accordion>
             ) : (
               <Stack align="start" spacing={4} flex="1" mr={4}>
                 {LinksToNoAuth.map((link) => (
@@ -300,7 +249,6 @@ const Navbar = () => {
               </Stack>
             )}
           </DrawerBody>
-
           <DrawerFooter>
             {isAuthenticated ? (
               <Link to="/login">
